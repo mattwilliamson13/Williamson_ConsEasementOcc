61ff37ad3dd1aff2ab176bab12c691113ceececc
mattwilliamson13 <mwilliamson@ucdavis.edu>
2019-06-10 13:19:24 -0700

    make matx sym

--- Scripts/gen_sp_precMatx.R
+++ Scripts/gen_sp_precMatx.R
@@ -7,9 +7,10 @@ library(spdep)
 gen_sp_precMatx <- function(geog, rho, tau){
   geog.order <- geog[order(geog$GEOID),] %>% as(., "Spatial");
   coords.geog <- coordinates(geog.order);
-  geog.nearnb <- knn2nb(knearneigh(coords.geog, k = 1), row.names = geog.order$GEOID); #estimate distance to first neareset neighbor
+  geog.nearnb <- knn2nb(knearneigh(coords.geog, k = 1), row.names = geog.order$GEOID, sym=TRUE); #estimate distance to first neareset neighbor
   dis.nb.list <- dnearneigh(coords.geog, 0,  max( unlist(nbdists(geog.nearnb, coords.geog))),row.names = geog.order$GEOID); #neighbors within the minimum distance to ensure at least 1 neighbor
   geog.W.matx <- nb2mat(dis.nb.list, style="B", zero.policy = TRUE); #converts list of neighbors to matrix with 0s on diagonal (i.e., no self-neighbros)
-  D.geog <- diag(rowSums(geog.W.mat)); #creates a matrix with the number of neighbors as the diagonal
+  D.geog <- diag(rowSums(geog.W.matx)); #creates a matrix with the number of neighbors as the diagonal
   prec.matx <- tau*(D.geog - rho*geog.W.matx)
+  return(list(geog.W.matx, prec.matx))
 }
