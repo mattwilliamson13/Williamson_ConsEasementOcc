f0b4d3696d415d779b86b8961d78a86008a8323b
mattwilliamson13 <mwilliamson@ucdavis.edu>
2019-06-10 13:35:51 -0700

    create shell

---
+++ lhsrun.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -J LHSSim
+#SBATCH --cpus-per-task=6
+#SBATCH --partition=med
+#SBATCH --mem=5Gb
+#SBATCH --time=02:00:00
+#SBATCH --mail-user=mwilliam@ucdavis.edu
+#SBATCH --mail-type=ALL
+
+SCRATCH=/scratch/$USER/$SLURM_JOB_ID
+echo $SLURM_SUBMIT_DIR
+echo Creating temp dir $SCRATCH
+srun mkdir -p $SCRATCH || exit $?
+srun cp -r $SLURM_SUBMIT_DIR/Scripts/  $SCRATCH || exit $?
+
+module load R
+hostname -f
+echo $SLURM_ARRAY_TASK_ID
+
+cd $SCRATCH
+srun ls
+Rscript --vanilla ./Scripts/onerun.R ${1} ${2}
+
+srun cp -r $SCRATCH/*.rds $SLURM_SUBMIT_DIR/Outputs/
+
+echo Removing $SCRATCH
+srun rm -rf $SCRATCH || exit $?
